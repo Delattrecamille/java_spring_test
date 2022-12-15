0a7afc81078b3fa016668eba871c399834272ba2
camille-tech <delattre_camille@hotmail.fr>
2022-12-15 17:52:06 +0100

    tes

---
+++ src/main/java/com/example/java_spring_test/JavaSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTestApplication.class, args);
+	}
+
+}
